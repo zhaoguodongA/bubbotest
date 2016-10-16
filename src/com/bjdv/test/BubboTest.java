99bafb0fbfa771b08477dc0bed4ef52c87619c34
john <john@john-PC>
2016-10-16 22:11:57 +0800

    添加System.out.println("22222222");

--- src/com/bjdv/test/BubboTest.java
+++ src/com/bjdv/test/BubboTest.java
@@ -5,6 +5,7 @@ public class BubboTest {
 	public static void main(String[] args) {
        System.out.println("");
        System.out.println("11111111");
+       System.out.println("22222222");
 	}
 
 }
