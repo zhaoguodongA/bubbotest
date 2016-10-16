1ba4ccfd4f01b8c1e7524b00aa5744da0a4b10c4
john <john@john-PC>
2016-10-16 21:53:23 +0800

    第一次修改dubbodev分支

--- src/com/bjdv/test/BubboTest.java
+++ src/com/bjdv/test/BubboTest.java
@@ -3,8 +3,8 @@ package com.bjdv.test;
 public class BubboTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+       System.out.println("");
+       
 	}
 
 }
