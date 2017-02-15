fb4477b229b6100b1f9ce9dcdb0ed1fc3b4a0bd2
zhaoguodong02 <zhaoguodong02@meituan.com>
2017-02-15 14:24:27 -0800

    删除打印输出

--- src/com/bjdv/test/BubboTest.java
+++ src/com/bjdv/test/BubboTest.java
@@ -3,9 +3,7 @@ package com.bjdv.test;
 public class BubboTest {
 
 	public static void main(String[] args) {
-       System.out.println("");
-       System.out.println("11111111");
-       System.out.println("22222222");
+
 	}
 
 }
