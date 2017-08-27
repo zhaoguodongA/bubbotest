c2dae4fd63ac85c6e38c91e2d15ea4a20f417382
赵国栋 <zhaoguodong02@dianping.com>
2017-08-27 12:09:38 +0800

    配置文件idea修改

--- src/com/bjdv/test/BubboTest.java
+++ src/com/bjdv/test/BubboTest.java
@@ -4,6 +4,7 @@ public class BubboTest {
 
 	public static void main(String[] args) {
       System.out.println("测试一idea");
+		System.out.println("测试一idea");
 	}
 
 }
--- src/com/bjdv/test/MacIdea.java
+++ src/com/bjdv/test/MacIdea.java
@@ -4,4 +4,6 @@ package com.bjdv.test;
  * Created by zhaoguodong on 2017/2/19.
  */
 public class MacIdea {
+
+
 }
